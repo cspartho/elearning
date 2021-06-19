55733915f15d9f44d1f50e23632344f0720c9c03
cspartho <cspartho19@gmail.com>
2021-06-20 00:20:27 +0600

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Distance Learning
+An elearning platform where instructor can add course materials and student can enroll course.A group chat server is added for students to discuss course topics.
---
+++ config/urls.py
@@ -0,0 +1,20 @@
+from django.contrib import admin
+from django.contrib.auth import views as auth_views
+from django.urls import include, path
+
+from courses.views import CourseListView
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('accounts/login/',auth_views.LoginView.as_view(),name='login'),
+    path('accounts/loggedout/',auth_views.LogoutView.as_view(),name='logout'),
+    path('admin/', admin.site.urls),
+    path('course/',include('courses.urls')),
+    path('',CourseListView.as_view(),name='course_list'),
+    path('students/',include('students.urls')),
+    path('chat/', include('chat.urls', namespace='chat')),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
